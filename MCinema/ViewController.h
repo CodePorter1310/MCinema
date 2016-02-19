067f2ad4e0f99a02a38341dc891c3ea256e7dd43
bever <bever@beverdeimac-105.local>
2016-02-19 15:50:27 +0800

    Initial Commit

---
+++ MCinema/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MCinema
+//
+//  Created by bever on 16/2/19.
+//  Copyright © 2016年 MN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MCinema/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MCinema
+//
+//  Created by bever on 16/2/19.
+//  Copyright © 2016年 MN. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
